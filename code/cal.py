c16a7f4c3a9fec71c54958a5828515ab48e2e0a5
ShiyuLiang <sliang26@illinois.edu>
2017-06-05 12:47:00 -0500

    Add files via upload

--- code/cal.py
+++ code/cal.py
@@ -30,13 +30,7 @@ transform = transforms.Compose([
 ])
 
 
-testset10 = torchvision.datasets.CIFAR10(root='../data', train=False, download=True, transform=transform)
-testloader10 = torch.utils.data.DataLoader(testset10, batch_size=1,
-                                         shuffle=False, num_workers=2)
 
-testset100 = torchvision.datasets.CIFAR100(root='../data', train=False, download=True, transform=transform)
-testloader100 = torch.utils.data.DataLoader(testset100, batch_size=1,
-                                           shuffle=False, num_workers=2)
 
 # loading neural network
 
